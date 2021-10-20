98f3197eb9de6a451924d563515b61418efd2c37
JonJaryongLee <director9303@gmail.com>
2021-10-20 17:12:24 +0900

    211020

---
+++ js/1-memory-game.js
@@ -0,0 +1,64 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // card options
+  const cardArray = [
+    {
+      name: "cat",
+      img: "../public/1-memory-game/cat.jpg",
+    },
+    {
+      name: "cat",
+      img: "../public/1-memory-game/cat.jpg",
+    },
+    {
+      name: "dog",
+      img: "../public/1-memory-game/dog.jpeg",
+    },
+    {
+      name: "dog",
+      img: "../public/1-memory-game/dog.jpeg",
+    },
+    {
+      name: "elephant",
+      img: "../public/1-memory-game/elephant.jpg",
+    },
+    {
+      name: "elephant",
+      img: "../public/1-memory-game/elephant.jpg",
+    },
+    {
+      name: "hedgehog",
+      img: "../public/1-memory-game/hedgehog.jpg",
+    },
+    {
+      name: "hedgehog",
+      img: "../public/1-memory-game/hedgehog.jpg",
+    },
+    {
+      name: "pig",
+      img: "../public/1-memory-game/pig.jpg",
+    },
+    {
+      name: "pig",
+      img: "../public/1-memory-game/pig.jpg",
+    },
+    {
+      name: "squirrel",
+      img: "../public/1-memory-game/squirrel.jpg",
+    },
+    {
+      name: "squirrel",
+      img: "../public/1-memory-game/squirrel.jpg",
+    },
+  ];
+
+  const grid = document.querySelector(".grid");
+
+  // create your board
+  const createBoard = () => {
+    for (let i = 0; i < cardArray.length; i++) {
+        let card = document.createElement("img");
+        card.setAttribute("src", )
+    }
+  };
+
+});
